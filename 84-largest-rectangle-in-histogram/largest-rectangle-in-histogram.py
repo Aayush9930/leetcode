49a0ddb5b853b294482eccb3b18b51c4d6397aee
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-02 19:06:41 -0500

    Time: 211 ms (17.88%) | Memory: 37 MB (7.30%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -0,0 +1,53 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stk = []
+        max_area = 0 
+        for i, height in enumerate(heights):
+            if stk:
+                temp = None
+                while stk and stk[-1][1] > height:
+                    temp = stk.pop()
+                    max_area = max(max_area, temp[1] * (i - temp[0]))
+                
+                stk.append([temp[0] if temp != None else i, height])
+                
+            else:
+                stk.append([i, height])
+
+        if stk:
+            while stk:
+                x = stk.pop()
+                max_area = max(max_area, x[1] * (len(heights) - x[0]))
+        
+        return max_area
+
+
+
+        # if len(heights) == 1:
+        #     return heights[0]
+        # if len(heights) == 0:
+        #     return 0
+
+        # out = []
+
+        # for i in range(len(heights)):
+        #     if i == 0:
+        #         r = i + 1
+        #         while r < len(heights) and heights[r] >= heights[i]:
+        #             r += 1 
+        #         out.append(heights[i] * (r - i))
+            
+        #     elif i == len(heights) - 1:
+        #         l = i - 1
+        #         while l >= 0 and heights[l] >= heights[i]:
+        #             l -= 1
+        #         out.append(heights[i] * (i - l))
+
+        #     else:
+        #         r, l = i + 1, i - 1
+        #         while l >= 0 and heights[l] >= heights[i]:
+        #             l -= 1
+        #         while r < len(heights) and heights[r] >= heights[i]:
+        #             r += 1 
+        #         out.append(heights[i] * (r - l - 1))
+        # return max(out)
