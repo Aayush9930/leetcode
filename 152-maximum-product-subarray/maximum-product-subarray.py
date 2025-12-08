1c4afd9394cdf1d5254f6a84543f5b50b2d513c9
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-08 16:38:51 -0500

    Time: 23 ms (5.93%) | Memory: 18.7 MB (11.88%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        maxProd, minProd = 1, 1
+        
+        res = nums[0]
+
+        for num in nums:
+            if num == 0:
+                maxProd, minProd = 1, 1
+
+            tmp = maxProd * num
+            maxProd = max(maxProd * num, minProd * num, num) # [-1,8]
+            minProd = min(tmp, minProd * num, num)
+            print(maxProd, minProd)
+            res = max(res, maxProd)
+        
+        return res
+        
+            
