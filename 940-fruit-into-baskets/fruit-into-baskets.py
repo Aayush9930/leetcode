3cfdc29d1e2a40b8ec89bb89947cbf9dc74a9541
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-12 16:11:11 -0500

    Time: 213 ms (40.28%) | Memory: 23.6 MB (42.24%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,23 +1,19 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        count = defaultdict(int)
+        h = {}
         l = 0
         res = 0
-        
-        for r, x in enumerate(fruits):
-            count[x] += 1
-            
-            # shrink window if we have more than 2 types
-            while len(count) > 2:
-                left_fruit = fruits[l]
-                count[left_fruit] -= 1
-                if count[left_fruit] == 0:
-                    del count[left_fruit]
-                l += 1
+        for r in range(len(fruits)):
+            h[fruits[r]] = 1 + h.get(fruits[r], 0)
+
+            if len(h) > 2:
+                while len(h) > 2:
+                    h[fruits[l]] -= 1
+                    if h[fruits[l]] == 0:
+                        del h[fruits[l]]
+                    l += 1
             
             res = max(res, r - l + 1)
         
         return res
-            
-                    
-                    
+
