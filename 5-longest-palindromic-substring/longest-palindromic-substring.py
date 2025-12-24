7d13b487a08f64aec9097409b57d61307e1f24d9
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-24 16:19:31 -0500

    Time: 307 ms (56.82%) | Memory: 17.3 MB (96.90%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,25 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-
-        max_len = 0
-        max_str = "" 
+        longest = 0
+        longest_str = ''
         for i in range(len(s)):
-            # odd len str
             l, r = i, i
-            while l >= 0 and r < len(s) and s[r] == s[l]:
-                if (r - l + 1) > max_len:
-                    max_len = (r - l + 1)
-                    max_str = s[l : r + 1]
+            while (l >= 0) and (r < len(s)) and (s[l] == s[r]):
+                if (r - l + 1) > longest:
+                    longest = (r - l + 1)
+                    longest_str = s[l: r + 1]
+                l -=1
                 r += 1
-                l -= 1
             
-            # even len str
             l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[r] == s[l]:
-                if (r - l + 1) > max_len:
-                    max_len = (r - l + 1)
-                    max_str = s[l : r + 1]
-                l -= 1
+            while (l >= 0) and (r < len(s)) and (s[l] == s[r]):
+                if (r - l + 1) > longest:
+                    longest = (r - l + 1)
+                    longest_str = s[l: r + 1]
+                l -=1
                 r += 1
+            
+        return longest_str
 
-        return max_str  
