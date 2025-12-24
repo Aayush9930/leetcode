563151a4ebc7628d018616c13d15bfd0d7be23ad
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-24 14:27:13 -0500

    Time: 7 ms (96.63%) | Memory: 17.9 MB (98.62%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,46 +1,47 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        invalid_set = set()
-        h = {}
-
-        def parse_string(s, i):
-            x = s.split(',')
-            x = {
-                "index": i,
+        def str_parser(s, i):
+            x = s.split(",")
+            res = {
                 "name": x[0],
                 "time": int(x[1]),
                 "amount": int(x[2]),
-                "city": x[3]
-            }
-            return x
-        
-        for i, tran in enumerate(transactions):
-            x = parse_string(tran, i)
+                "city": x[3],
+                "index": i
+            } 
+            return res
 
+        invalid_transactions = set()
+        h = {}
+
+        for i in range(len(transactions)):  
+            x = str_parser(transactions[i], i)
             if x["amount"] > 1000:
-                invalid_set.add(x['index'])
+                invalid_transactions.add(i)
             
-            if x["name"] in h:
-                h[x["name"]].append(x)
-
+            if x['name'] in h:
+                h[x['name']].append((x['time'], x['city'], x['index']))
             else:
-                h[x["name"]] = [x]
-        
+                h[x['name']] = [(x['time'], x['city'], x['index'])]
+
         for name in h:
-            h[name].sort(key=lambda tx: tx["time"])
-        
+            h[name].sort(key=lambda x: x[0]) 
+
         for name in h:
             for i in range(len(h[name])):
+                t1, c1, i1 = h[name][i]
                 for j in range(i + 1, len(h[name])):
-                    if h[name][j]["time"] - h[name][i]["time"] <= 60:
-                        if h[name][i]["city"] != h[name][j]["city"]:
-                            invalid_set.add(h[name][i]['index'])
-                            invalid_set.add(h[name][j]['index'])
-                    
-                    else:
+                    t2, c2, i2 = h[name][j]
+                    if abs(t2 -t1) <= 60 and c1 != c2:
+                        invalid_transactions.add(i1)
+                        invalid_transactions.add(i2)
+                    elif abs(t2 -t1) > 60:
                         break
         out = []
-        for index in invalid_set:
-            out.append(transactions[index])
+        for i in invalid_transactions:
+            out.append(transactions[i])
         
         return out
+                        
+
+
