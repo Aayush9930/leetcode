786b12586379b23c47666cbedf88a3c7fd060ddb
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-25 15:48:36 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (71.20%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,19 +1,20 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        return self._allPathsSourceTarget(graph, 0, len(graph) - 1)
+        return self._allPathsSourceTarget(graph, 0, len(graph) - 1, {})
 
-    def _allPathsSourceTarget(self, g, src, dst):
+    def _allPathsSourceTarget(self, g, src, dst, memo):
+        if src in memo:
+            return memo[src]
+            
         if src == dst:
             return [[ src ]]
-        
         if g[src] == []:
             return None
-        
         out = []
         for n in g[src]:
-            x = self._allPathsSourceTarget(g, n, dst)
+            x = self._allPathsSourceTarget(g, n, dst, memo)
             if x != None:
                 for p in x:
                     out.append([src, *p])
-
-        return out
+        memo[src] = out
+        return memo[src]
