5c8c384b77c7ccb786aafc45468f922b574b540e
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-24 22:26:26 -0500

    Time: 3 ms (98.57%) | Memory: 17.5 MB (98.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,14 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        h1 = {}
-        for c in s:
-            h1[c] = 1 + h1.get(c, 0)
-        
-        h2 = {}
-        for c in t:
-            h2[c] = 1 + h2.get(c, 0)
-        
-        if h1 == h2:
-            return True 
-        
-        return False 
+        return Counter(s) == Counter(t)
