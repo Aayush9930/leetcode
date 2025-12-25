8c406d217530b5a90a302f9acc326d584347ab23
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-24 21:54:48 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (91.51%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,18 +1,22 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        x = self._wordBreak(s, wordDict)
+        x = self._wordBreak(s, wordDict, 0, {})
         out = []
         for p in x:
             out.append(" ".join(p))
         return out
 
-    def _wordBreak(self, s, wordDict):
-        if s == '':
+    def _wordBreak(self, s, wordDict, i, memo):
+        if i in memo:
+            return memo[i]
+
+        if i == len(s):
             return [[]]
         out = []
         for word in wordDict:
-            if s.startswith(word):
-                x = self._wordBreak(s[len(word):], wordDict)
+            if s.startswith(word, i):
+                x = self._wordBreak(s, wordDict, i + len(word), memo)
                 for p in x:
                     out.append([word, *p])
-        return out
+        memo[i] = out
+        return memo[i]
