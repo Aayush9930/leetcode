4b75eca6ee9ff167bab56451a3bc99de24fbb1f4
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-25 19:10:50 -0500

    Time: 0 ms (100.00%) | Memory: 24.8 MB (48.29%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,22 +1,18 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1 
 
-        while l <= r:       
-            mid = (l + r) // 2          
-
-            if (mid == 0 or  nums[mid] != nums[mid - 1]) and (mid ==  len(nums) - 1 or nums[mid] != nums[mid + 1]):
+        while l <= r:
+            mid = (l + r) // 2
+            if (mid - 1 < 0 or nums[mid - 1] != nums[mid]) and (mid + 1 >= len(nums) or nums[mid] != nums[mid + 1]):
                 return nums[mid]
+            
+            left_size = mid - 1 if nums[mid - 1] == nums[mid] else mid 
 
+            if left_size % 2 == 0:
+                l = mid + 1 if nums[mid] == nums[mid - 1] else mid + 2
+            
             else:
-                left_size = mid - l - 1 if nums[mid] == nums[mid - 1] else mid - l
-        
-                if left_size % 2 == 0:
-                    l = mid + 1 if nums[mid] != nums[mid + 1] else mid + 2
+                r = mid - 1 if nums[mid] == nums[mid + 1] else mid - 2
 
-                else:
-                        r = mid - 1 if nums[mid] != nums[mid - 1] else mid - 2
-
-
-
-
+            
