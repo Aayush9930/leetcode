45dcac728569ad808eae8478d9c09d71f0d5baf1
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-25 21:09:06 -0500

    Time: 38 ms (91.51%) | Memory: 17.3 MB (97.21%) - LeetSync

--- 1233-number-of-ships-in-a-rectangle/number-of-ships-in-a-rectangle.py
+++ 1233-number-of-ships-in-a-rectangle/number-of-ships-in-a-rectangle.py
@@ -12,27 +12,47 @@
 
 class Solution:
     def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
-        if topRight.x < bottomLeft.x or topRight.y < bottomLeft.y:
-            return 0
-        elif topRight.x == bottomLeft.x and topRight.y == bottomLeft.y:
-            if sea.hasShips(topRight, bottomLeft) == True:
-                return 1
-            return 0
+        if topRight.x == bottomLeft.x and topRight.y == bottomLeft.y:
+            return 1 if sea.hasShips(topRight, bottomLeft) == True else 0 
+        elif topRight.x < bottomLeft.x or topRight.y < bottomLeft.y:
+            return 0 
         if not sea.hasShips(topRight, bottomLeft):
-                return 0
+            return 0
 
-        
-        mid_x = (topRight.x + bottomLeft.x) // 2 
-        mid_y = (topRight.y + bottomLeft.y) // 2 
+        midx = (topRight.x + bottomLeft.x) // 2 
+        midy = (topRight.y + bottomLeft.y) // 2 
 
-        topRQ = self.countShips(sea, topRight, Point(mid_x + 1, mid_y + 1))
-        topLQ = self.countShips(sea, Point(mid_x, topRight.y), Point(bottomLeft.x, mid_y + 1))
-        bottomLQ = self.countShips(sea, Point(mid_x, mid_y), bottomLeft)
-        bottomRQ = self.countShips(sea, Point(topRight.x, mid_y), Point(mid_x + 1, bottomLeft.y))
+        ul = self.countShips(sea, Point(midx, topRight.y), Point(bottomLeft.x, midy + 1))
+        ur = self.countShips(sea, topRight, Point(midx + 1, midy + 1))
+        ll = self.countShips(sea, Point(midx, midy), bottomLeft)
+        lr = self.countShips(sea, Point(topRight.x, midy), Point(midx + 1, bottomLeft.y))
 
-        return topLQ + topRQ + bottomLQ + bottomRQ
-
+        return ul + ur + ll + lr
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
