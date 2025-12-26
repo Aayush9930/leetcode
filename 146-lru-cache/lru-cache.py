852b2b1a2c78ad367bf1d21b0fd6d1e6392ef609
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-26 16:43:26 -0500

    Time: 169 ms (22.63%) | Memory: 77.8 MB (92.53%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,33 +1,20 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key = None, val = None):
         self.key = key
-        self.val = val 
+        self.val = val
         self.next = None
-        self.prev = None  
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
         self.h = {}
-        self.l = Node(0, 0)
-        self.r = Node(0, 0)
-        
-        self.l.next = self.r
-        self.r.prev = self.l
-    # i am adding the node on the right -> mru
-    def add(self, node):
-        p, nxt = self.r.prev, self.r
-        p.next = node
-        nxt.prev = node
-        node.prev = p
-        node.next = nxt
-
-    def remove(self, node):
-        p, nxt = node.prev, node.next
-        p.next = nxt
-        nxt.prev = p
-
+        self.left = Node()
+        self.right = Node()
+        self.left.next = self.right
+        self.right.prev = self.left
+        self.capacity = capacity
+    
     def get(self, key: int) -> int:
         if key not in self.h:
             return -1
@@ -35,19 +22,30 @@ class LRUCache:
         self.add(self.h[key])
         return self.h[key].val
 
+    def add(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = node
+        nxt.prev = node
+        node.next = nxt
+        node.prev = prev
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
     def put(self, key: int, value: int) -> None:
         if key in self.h:
             self.h[key].val = value
             self.remove(self.h[key])
             self.add(self.h[key])
-        
         else:
             self.h[key] = Node(key, value)
             self.add(self.h[key])
             if len(self.h) > self.capacity:
-                lru = self.l.next
-                self.remove(lru)
+                lru = self.left.next
                 del self.h[lru.key]
+                self.remove(lru)
 
 
 
