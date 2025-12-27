d0458744b5bf920377f3860a88859912d513b9e9
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-27 00:26:39 -0500

    Time: 39 ms (98.18%) | Memory: 19 MB (79.95%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -7,31 +7,29 @@ class Node:
         self.right = right
         self.next = next
 """
-from collections import deque
+from collections import deque 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if root == None:
+        if not root:
             return root
-        
+            
+        tree = []
         q = deque([(root, 0)])
 
         while q:
             n, l = q.popleft()
+            if len(tree) == l:
+                tree.append([])
+            tree[l].append(n)
 
             if n.left:
                 q.append((n.left, l + 1))
+            
             if n.right:
                 q.append((n.right, l + 1))
 
-            curr = n
-            while q and q[0][1] == l:
-                new, l = q.popleft()
-                if new.left:
-                    q.append((new.left, l + 1))
-                if new.right:
-                    q.append((new.right, l + 1))
-                curr.next = new
-                curr = new
-            curr.next = None
-
+        for layer in tree:
+            for i in range(len(layer)):
+                layer[i].next = layer[i + 1] if i + 1 < len(layer) else None
+        
         return root
