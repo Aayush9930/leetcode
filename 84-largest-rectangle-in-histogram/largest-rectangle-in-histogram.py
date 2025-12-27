6de15de3cd7908066df94d84985b83a9a59b422b
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-26 21:31:02 -0500

    Time: 163 ms (44.55%) | Memory: 35.3 MB (29.22%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -2,99 +2,23 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         stk = []
         max_area = 0
-        
         for i in range(len(heights)):
             if stk:
-                temp = None
-                while stk and stk[-1][1] > heights[i]:
-                    area = stk[-1][1] * (i - stk[-1][0])
-                    max_area = max(max_area, area)
-                    temp = stk.pop()
-                
-                if temp == None:
-                    stk.append((i, heights[i]))
+                if stk[-1][1] > heights[i]:
+                    index = None
+                    while stk and stk[-1][1] > heights[i]:
+                        idx, h = stk.pop()
+                        area = h * (i - idx)
+                        max_area = max(max_area, area)
+                        index = idx
+                    stk.append((index, heights[i]))
                 else:
-                    stk.append((temp[0], heights[i]))
+                    stk.append((i, heights[i]))
             else:
                 stk.append((i, heights[i]))
-        
-        for t in stk:
-            area = (len(heights) - t[0]) * t[1]
+
+        for i, h in stk:
+            area = h * (len(heights) - i)
             max_area = max(max_area, area)
 
-        return max_area
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # stk = []
-        # max_area = 0 
-        # for i, height in enumerate(heights):
-        #     if stk:
-        #         temp = None
-        #         while stk and stk[-1][1] > height:
-        #             temp = stk.pop()
-        #             max_area = max(max_area, temp[1] * (i - temp[0]))
-                
-        #         stk.append([temp[0] if temp != None else i, height])
-                
-        #     else:
-        #         stk.append([i, height])
-
-        # if stk:
-        #     while stk:
-        #         x = stk.pop()
-        #         max_area = max(max_area, x[1] * (len(heights) - x[0]))
-        
-        # return max_area
-
-
-
-        # if len(heights) == 1:
-        #     return heights[0]
-        # if len(heights) == 0:
-        #     return 0
-
-        # out = []
-
-        # for i in range(len(heights)):
-        #     if i == 0:
-        #         r = i + 1
-        #         while r < len(heights) and heights[r] >= heights[i]:
-        #             r += 1 
-        #         out.append(heights[i] * (r - i))
-            
-        #     elif i == len(heights) - 1:
-        #         l = i - 1
-        #         while l >= 0 and heights[l] >= heights[i]:
-        #             l -= 1
-        #         out.append(heights[i] * (i - l))
-
-        #     else:
-        #         r, l = i + 1, i - 1
-        #         while l >= 0 and heights[l] >= heights[i]:
-        #             l -= 1
-        #         while r < len(heights) and heights[r] >= heights[i]:
-        #             r += 1 
-        #         out.append(heights[i] * (r - l - 1))
-        # return max(out)
+        return max_area 
