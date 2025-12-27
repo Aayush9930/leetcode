5bbf27a64b9fd8cedb1200b694ca5d288ed0bfa4
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-26 21:06:11 -0500

    Time: 32 ms (5.65%) | Memory: 18.2 MB (95.94%) - LeetSync

--- 1775-design-an-ordered-stream/design-an-ordered-stream.py
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.py
@@ -1,21 +1,19 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.l = [0] * n
+        self.lst = [ None ] * n
         self.visited = set()
 
     def insert(self, idKey: int, value: str) -> List[str]:
-        self.l[idKey - 1] = value
+        self.lst[idKey - 1] = value
         out = []
-        i = 0 
-        while i < len(self.l) and self.l[i] != 0:
-            if self.l[i] not in self.visited:
-                out.append(self.l[i])
-                self.visited.add(self.l[i])
+        i = 0
+        while i < len(self.lst) and self.lst[i] != None:
+            if self.lst[i] not in self.visited:
+                self.visited.add(self.lst[i])
+                out.append(self.lst[i])
             i += 1
         return out
-            
-
 
 # Your OrderedStream object will be instantiated and called as such:
 # obj = OrderedStream(n)
