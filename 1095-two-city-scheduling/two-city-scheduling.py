5788ae98e90c8999bb7a9cc0618f3c11caa44102
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-27 19:43:49 -0500

    Time: 20 ms (5.24%) | Memory: 17.6 MB (90.75%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,27 +1,30 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        n = int(len(costs) / 2)
-        return self._twoCitySchedCost(costs, n, n, 0, {})
-    
-    def _twoCitySchedCost(self, cost, na, nb, i, memo):
-        m = (na, nb)
-        if m in memo:
-            return memo[m]
+        return self._twoCitySchedCost(costs, len(costs) // 2, len(costs) // 2, 0, {})
 
-        if na == 0 and nb == 0:
+    def _twoCitySchedCost(self, costs, c1, c2, i, memo):
+        r = (c1, c2, i)
+        if r in memo:
+            return memo[r]
+
+        if i == len(costs):
             return 0
         
-        first = cost[i]
         out = []
-        if na > 0:
-            x = first[0] + self._twoCitySchedCost(cost, na -1, nb, i + 1, memo)
+
+        if c1 > 0:
+            x = costs[i][0] + self._twoCitySchedCost(costs, c1 - 1, c2, i + 1, memo)
             out.append(x)
         
-        if nb > 0:
-            y = first[1] + self._twoCitySchedCost(cost, na, nb - 1, i + 1, memo)
+        if c2 > 0:
+            y = costs[i][1] + self._twoCitySchedCost(costs, c1, c2 - 1, i + 1, memo)
             out.append(y)
+
+        memo[r] =  min(out)
+        return memo[r]
         
-        memo[m] =  min(out)
-        return memo[m]
 
 
+
+        
+        
