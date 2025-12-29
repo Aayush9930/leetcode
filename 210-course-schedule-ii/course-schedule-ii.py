67912ffdb562671a650dd90ca308937247489d50
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-28 22:39:57 -0500

    Time: 3 ms (76.19%) | Memory: 18.4 MB (96.62%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,40 +1,43 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        if prerequisites == []:
+            return [ i for i in range(numCourses) ]
+
         g = {}
+
         for i in range(numCourses):
             g[i] = []
         
-        for edge in prerequisites:
-            x, y = edge
-            g[y].append(x)
-        
 
+        for x, y in prerequisites:
+            g[y].append(x)
+            
         num_parents = {}
 
         for i in range(numCourses):
             num_parents[i] = 0
-
-        for node in g:
-            for nei in g[node]:
-                num_parents[nei] = 1 + num_parents.get(nei, 0)
-        print(num_parents)
         
+        for node in g:
+            for n in g[node]:
+                num_parents[n] += 1
+
         ready = [ node for node in num_parents if num_parents[node] == 0 ]
+
         out = []
         while ready:
             node = ready.pop()
             out.append(node)
 
-            for nei in g[node]:
-                num_parents[nei] -= 1
-                if num_parents[nei] == 0:
-                    ready.append(nei)
+            for n in g[node]:
+                num_parents[n] -= 1
+                if num_parents[n] == 0:
+                    ready.append(n)
 
-        for node in num_parents:
-            if num_parents[node] > 0:
-                return []
-
-        return out 
+        if len(set(num_parents.values())) != 1:
+            return []
+        
+        return out
             
 
-        
+
+
