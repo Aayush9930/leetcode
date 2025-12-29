9cab123928eed14a0fb6675ee460e4506c97f9d8
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-29 16:44:55 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.65%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,34 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        # 1st: finding the break point index 
+        bp = None
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                bp = i
+                break
+        
+        if bp == None:
+            return nums.sort()
+        
+        # 2nd: from [bp + 1 : ] find the smallest element greater than bp element 
+        
+        for i in range(len(nums) - 1, bp, -1):
+            if nums[bp]  < nums[i]:
+                nums[bp], nums[i] = nums[i], nums[bp]
+                break
+        
+        # reverse [bp + 1 : ]
+        l, r = bp + 1, len(nums) - 1
+
+        while l <= r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        
+        
+
+     
