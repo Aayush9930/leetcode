b56da2340061f897b2db0f5c17cefa6754f6bef4
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-29 16:49:10 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (95.53%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -6,29 +6,36 @@ class Solution:
 
         # 1st: finding the break point index 
         bp = None
-        for i in range(len(nums) - 2, -1, -1):
+        for i in range(len(nums) - 2, -1, -1):  # O(n)
             if nums[i] < nums[i + 1]:
                 bp = i
                 break
         
         if bp == None:
-            return nums.sort()
+            l, r = 0, len(nums) - 1
+ 
+            while l <= r:    #O(n)
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+            return
+
         
         # 2nd: from [bp + 1 : ] find the smallest element greater than bp element 
         
-        for i in range(len(nums) - 1, bp, -1):
+        for i in range(len(nums) - 1, bp, -1):  #O(n)
             if nums[bp]  < nums[i]:
                 nums[bp], nums[i] = nums[i], nums[bp]
                 break
         
-        # reverse [bp + 1 : ]
+        # 3rd: reverse [bp + 1 : ]
         l, r = bp + 1, len(nums) - 1
-
-        while l <= r:
+ 
+        while l <= r:    #O(n)
             nums[l], nums[r] = nums[r], nums[l]
             l += 1
             r -= 1
         
-        
+
 
      
