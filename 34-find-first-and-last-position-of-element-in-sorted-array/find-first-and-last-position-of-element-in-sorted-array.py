2e5a83bf75a01102ad42a54c8f6f611bdb7483d9
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-29 17:41:39 -0500

    Time: 0 ms (100.00%) | Memory: 18.6 MB (90.96%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,25 +1,27 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l, r = 0, len(nums) - 1
+        def bound_calc(leftBias):
+            l, r  = 0, len(nums) - 1 
+            bound = -1
+            while l <= r:
+                mid = (l + r) // 2
 
-        while l <= r:
-            mid = (l + r) // 2
+                if target > nums[mid]:
+                    l = mid + 1
 
-            if target > nums[mid]:
-                l = mid + 1
-
-            elif target < nums[mid]:
-                r = mid - 1 
-
-            else:
-                l, r = mid, mid
-                while l >= 0 and nums[l] == nums[mid]:
-                    l -= 1
-                while r < len(nums) and nums[r] == nums[mid]:
-                    r += 1
+                elif target < nums[mid]:
+                    r = mid - 1
                 
-                return [l + 1, r - 1]
+                else:
+                    bound = mid
+                    if leftBias:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+            return bound
             
-        return [-1, -1]
+        left_bound = bound_calc(True)
+        right_bound =  bound_calc(False)
 
-                 
+        return [ left_bound, right_bound ]
+
