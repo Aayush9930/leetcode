f13260ef6b52f0022b4638e536bd528c30f91238
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-28 20:30:52 -0500

    Time: 27 ms (92.21%) | Memory: 32.3 MB (98.70%) - LeetSync

---
+++ 148-sort-list/sort-list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        vals = []
+        curr = head
+
+        while curr:
+            vals.append(curr.val)
+            curr = curr.next
+        
+        vals.sort()
+        curr = head
+        for v in vals:
+            curr.val = v
+            curr = curr.next
+        
+        return head
