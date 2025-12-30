ab6a22af31a834d507efba7f308a66a04d6b821b
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-30 10:53:06 -0500

    Time: 1 ms (39.31%) | Memory: 17.6 MB (85.32%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -8,35 +8,34 @@ class Solution:
 
         if i == len(s):
             return 1
-        
-        if s[i] == '0':
+
+        if s[i] == "0":
             return 0
         
-        if s[i] == '1':
+        if s[i] == "1":
             out = []
-            x = self._numDecodings(s, i + 1, memo)
-            out.append(x)
-            
             if i + 1 < len(s):
                 y = self._numDecodings(s, i + 2, memo)
                 out.append(y)
-            
+
+            x = self._numDecodings(s, i + 1, memo)
+            out.append(x)
             memo[i] = sum(out)
             return memo[i]
         
-        if s[i] == '2':
+        elif s[i] == "2":
             out = []
-            x = self._numDecodings(s, i + 1, memo)
-            out.append(x)
-            
-            if i + 1 < len(s) and 0 <= int(s[i + 1]) <= 6:
+            if i + 1 < len(s) and (0 <= int(s[i + 1]) <= 6):  
                 y = self._numDecodings(s, i + 2, memo)
                 out.append(y)
-            
+            x = self._numDecodings(s, i + 1, memo)
+            out.append(x)
             memo[i] =  sum(out)
             return memo[i]
+        
         else:
-            memo[i] =  self._numDecodings(s, i + 1, memo)
+            memo[i] = self._numDecodings(s, i + 1, memo)
             return memo[i]
+        
 
-            
+
