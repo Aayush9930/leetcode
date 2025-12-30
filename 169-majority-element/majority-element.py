eba7ad9e31ec8d48bf591aea6da5c9b6587fd7bd
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-30 14:49:49 -0500

    Time: 7 ms (58.22%) | Memory: 19.2 MB (80.20%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+        # Apprach 1 to achieve O(1) space
+        # nums.sort()
+        # return nums[len(nums) // 2]
+
+        count = 0
+        candidate = None
+
+        for x in nums:
+            if count == 0:
+                candidate = x
+                count = 1
+            
+            elif x == candidate:
+                count += 1
+            
+            elif x != candidate:
+                count -= 1
+        return candidate
+
+
