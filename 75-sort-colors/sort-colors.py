cead9c31f5dd5ef4eb23ac5c9796d31d00298b6a
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-29 23:16:25 -0500

    Time: 1 ms (14.89%) | Memory: 17.6 MB (81.09%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        count = [0, 0, 0]
+        for num in nums:
+            count[num] += 1
+
+        i = 0
+        for color in range(3):
+            for _ in range(count[color]):
+                nums[i] = color
+                i += 1
