9c0c9c719e9042666f6ace9a6ea2b7c578b585f3
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-30 10:12:26 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (88.01%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,16 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = [ 0, 0, 0 ]
-        for num in nums:
-            count[num] += 1
+        # Partition Sort -> 2 pass
+        l = 0 
+        
+        for r in range(len(nums)):   # [0, 0, .....]
+            if nums[r] == 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
 
-        i = 0
-        for j in range(3):
-            while count[j] != 0:
-                nums[i] = j
-                i += 1
-                count[j] -= 1
+        for r in range(l, len(nums)):
+            if nums[r] == 1:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            
+        
+        
+        
+        # Count Sort / Bucket Sort
+        # count = [ 0, 0, 0 ]
+        # for num in nums:
+        #     count[num] += 1
+
+        # i = 0
+        # for j in range(3):
+        #     while count[j] != 0:
+        #         nums[i] = j
+        #         i += 1
+        #         count[j] -= 1
         
 
 
