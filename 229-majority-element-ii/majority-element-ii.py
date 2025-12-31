17dc3fba183ab441968a6c5e9d13e8da005363d0
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-31 11:23:42 -0500

    Time: 15 ms (10.28%) | Memory: 19.9 MB (96.45%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -3,12 +3,41 @@ class Solution:
         f = {}
 
         for x in nums:
+            # blindly adding
             f[x] = 1 + f.get(x, 0)
+
+            # cleaning up extra elements
+            if  len(f) > 2:
+                remove_elements = []
+                for val in f:
+                    f[val] -= 1
+                    if f[val] == 0:
+                        remove_elements.append(val)
+
+                for val in remove_elements:
+                    del f[val]
+                
+        if len(f) == 0:
+            return []
         
         out = []
-        for x in f:
-            if f[x] > len(nums) // 3:
-                out.append(x)
-        
+        for val in f:
+            x = nums.count(val)
+            if x > len(nums) // 3:
+                out.append(val)
+
         return out
+
+        
+        # f = {}
+
+        # for x in nums:
+        #     f[x] = 1 + f.get(x, 0)
+        
+        # out = []
+        # for x in f:
+        #     if f[x] > len(nums) // 3:
+        #         out.append(x)
+        
+        # return out
          
