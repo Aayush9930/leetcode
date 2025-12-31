8682292c9810af122547f56ebebb21e3ef9b1c11
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-31 11:08:23 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (83.24%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,11 +1,30 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        count_row = 0
-        row_size = 1
+        l, r = 0, n
+        last_valid = None
+        while l <= r:
+            mid = (l + r) // 2 
+            size = int((mid * (mid + 1)) / 2)
 
-        while n >= row_size:
-            n = n - row_size
-            count_row += 1
-            row_size += 1
+            if n > size:
+                last_valid = mid
+                l = mid + 1
+            
+            elif n < size:
+                r = mid - 1
+                
+            else:
+                return mid
         
-        return count_row
+        return last_valid
+        
+        
+        # count_row = 0
+        # row_size = 1
+
+        # while n >= row_size:
+        #     n = n - row_size
+        #     count_row += 1
+        #     row_size += 1
+        
+        # return count_row
