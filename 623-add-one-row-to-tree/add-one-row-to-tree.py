ebe0cd7ff66e6d7bbba29f8471750a9c3d736f19
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-31 18:47:52 -0500

    Time: 1 ms (43.26%) | Memory: 18.6 MB (88.12%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        if depth == 1:
+            n = TreeNode(val)
+            n.left = root
+            return n
+
+
+        q = deque([(root, 1)])
+
+        while q:
+            n, l = q.pop()
+
+            if l == depth - 1:
+                temp_left = n.left
+                temp_right = n.right
+                n.left = TreeNode(val)
+                n.right = TreeNode(val)
+                n.left.left = temp_left
+                n.right.right = temp_right
+
+            else:
+                if n.left:
+                    q.append((n.left, l + 1))
+                
+                if n.right:
+                    q.append((n.right, l + 1))
+        return root
