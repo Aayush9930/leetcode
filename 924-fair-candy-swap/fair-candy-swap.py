e295c3d2cf723c9a2e70b84f76959dfcfe71deed
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-31 23:33:19 -0500

    Time: 3 ms (93.69%) | Memory: 19.5 MB (66.84%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.py
+++ 924-fair-candy-swap/fair-candy-swap.py
@@ -3,8 +3,7 @@ class Solution:
         a = set(aliceSizes)
         opt = (sum(aliceSizes) + sum(bobSizes)) // 2
         bobtotal = sum(bobSizes)
-
         for s in bobSizes:
-            if (opt - (bobtotal - s)) in aliceSizes:
+            if (opt - (bobtotal - s)) in a:
                 return [(opt - (bobtotal - s)), s]
         
