6511213997e812056c0cd0043a17a9350c5dc429
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2025-12-31 23:32:49 -0500

    Time: 1465 ms (22.08%) | Memory: 19.5 MB (66.84%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:   
+        a = set(aliceSizes)
+        opt = (sum(aliceSizes) + sum(bobSizes)) // 2
+        bobtotal = sum(bobSizes)
+
+        for s in bobSizes:
+            if (opt - (bobtotal - s)) in aliceSizes:
+                return [(opt - (bobtotal - s)), s]
+        
