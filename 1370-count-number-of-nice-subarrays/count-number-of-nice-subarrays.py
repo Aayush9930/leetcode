9d2fc80406e5e943229aadb4c918a7176c157c77
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2026-01-02 12:53:56 -0500

    Time: 89 ms (70.55%) | Memory: 22.7 MB (92.84%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,25 +1,25 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        count_odd = 0 
-        l = 0
-        m = 0
         res = 0
+        l, m = 0, 0 
+        odd = 0
 
         for r in range(len(nums)):
-            # increase my window till valid
+            # increase the window
             if nums[r] % 2 == 1:
-                    count_odd += 1
-
-            while count_odd > k:
-                if nums[l] % 2 == 1:
-                    count_odd -= 1
-                l += 1
-                m = l
-
-            if count_odd == k:
+                odd += 1
+            
+            # decrease my window if the window is invalid
+            if odd > k:
+                while odd > k:
+                    if nums[l] % 2 == 1:
+                        odd -= 1
+                    l += 1
+                    m = l
+                
+            if odd == k:
                 while nums[m] % 2 == 0:
                     m += 1
                 res += (m - l + 1)
-
+            
         return res
-                
