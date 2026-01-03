1f0bf93c8a05333d58adac402239de7002d28fbc
Aayush Kamlesh Mehta <151272377+Aayush9930@users.noreply.github.com>
2026-01-02 20:34:59 -0500

    Time: 329 ms (5.01%) | Memory: 35.1 MB (8.11%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,33 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        if len(s) == 1:
+            return [[ s[0] ]]
+        
+        first = s[0]
+        rest = self.partition(s[1:])
+
+        out = []
+        seen = set()
+        for p in rest:
+            out.append([first, *p])
+            string = first
+            for i in range(len(p)):
+                string = string + p[i]
+                if self.isPalindrome(string):
+                    incoming = [string] + p[i + 1:]
+                    if tuple(incoming) not in seen:
+                        seen.add(tuple(incoming))
+                        out.append([string] + p[i + 1:])
+        return out 
+
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
+        while l <= r:
+            if s[l] != s[r]:
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+
+
